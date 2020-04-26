2a14e04397e43d15624b43f5c8240564dfd39245
aptx113 <aptx113@gmail.com>
2020-04-27 01:37:27 +0800

    get all users data thorough GitHub Api

--- app/src/main/java/com/danteyu/studio/githubusersapp/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/danteyu/studio/githubusersapp/data/source/remote/RemoteDataSource.kt
@@ -1,16 +1,43 @@
 package com.danteyu.studio.githubusersapp.data.source.remote
 
+import com.danteyu.studio.githubusersapp.R
+import com.danteyu.studio.githubusersapp.data.AllUsersResult
 import com.danteyu.studio.githubusersapp.data.GitHubUser
 import com.danteyu.studio.githubusersapp.data.Result
 import com.danteyu.studio.githubusersapp.data.source.GitHubDataSource
+import com.danteyu.studio.githubusersapp.network.GitHubApi
+import com.danteyu.studio.githubusersapp.util.Logger
+import com.danteyu.studio.githubusersapp.util.Util.getString
+import com.danteyu.studio.githubusersapp.util.Util.isInternetAvailable
 
 /**
  * Created by George Yu on 2020/4/26.
  */
 
-object RemoteDataSource :GitHubDataSource{
+object RemoteDataSource : GitHubDataSource {
 
     override suspend fun getAllUsers(): Result<List<GitHubUser>> {
-        TODO("Not yet implemented")
+        if (!isInternetAvailable()) {
+            return Result.Fail(getString(R.string.internet_not_connected))
+        }
+        // Get the Deferred object for our Retrofit request
+        val getResultDeferred = GitHubApi.retrofitService.getAllGitHubUserAsync()
+        return try {
+            // this will run on a thread managed by Retrofit
+            val listResult = getResultDeferred.await()
+
+//            listResult.error?.let {
+//                return Result.Fail(it)
+//            }
+            Result.Success(listResult)
+//            listResult?.let {
+//                return Result.Success(it)
+//            }
+//            Result.Fail(getString(R.string.you_know_nothing))
+
+        } catch (e: Exception) {
+            Logger.w("[${this::class.simpleName}] exception=${e.message}")
+            Result.Error(e)
+        }
     }
 }
--- app/src/main/java/com/danteyu/studio/githubusersapp/home/HomeViewModel.kt
+++ app/src/main/java/com/danteyu/studio/githubusersapp/home/HomeViewModel.kt
@@ -3,15 +3,25 @@ package com.danteyu.studio.githubusersapp.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.danteyu.studio.githubusersapp.R
+import com.danteyu.studio.githubusersapp.data.GitHubUser
+import com.danteyu.studio.githubusersapp.data.Result
 import com.danteyu.studio.githubusersapp.data.source.GitHubRepository
 import com.danteyu.studio.githubusersapp.network.LoadApiStatus
 import com.danteyu.studio.githubusersapp.util.Logger
+import com.danteyu.studio.githubusersapp.util.Util.getString
+import kotlinx.coroutines.launch
 
 /**
  * Created by George Yu on 2020/4/26.
  */
 class HomeViewModel(private val gitHubRepository: GitHubRepository) : ViewModel() {
 
+    private val _users = MutableLiveData<List<GitHubUser>>()
+
+    val users: LiveData<List<GitHubUser>>
+        get() = _users
 
     private val _status = MutableLiveData<LoadApiStatus>()
 
@@ -28,5 +38,45 @@ class HomeViewModel(private val gitHubRepository: GitHubRepository) : ViewModel(
         Logger.i("[${this::class.simpleName}]${this}")
         Logger.i("------------------------------------")
 
+        getAllUsersResult(true)
+    }
+
+    private fun getAllUsersResult(isInitial: Boolean = false) {
+
+        viewModelScope.launch {
+
+            if (isInitial) _status.value = LoadApiStatus.LOADING
+            // It will return Result object after Deferred flow
+            val result = gitHubRepository.getAllUsers()
+
+            _users.value = when (result) {
+                is Result.Success -> {
+                    _error.value = null
+                    if (isInitial) _status.value = LoadApiStatus.DONE
+                    result.data
+                }
+                is Result.Fail -> {
+                    _error.value = result.error
+                    if (isInitial) _status.value = LoadApiStatus.ERROR
+                    null
+                }
+                is Result.Error -> {
+                    _error.value = result.exception.toString()
+                    if (isInitial) _status.value = LoadApiStatus.ERROR
+                    null
+                }
+                else -> {
+                    _error.value = getString(R.string.you_know_nothing)
+                    if (isInitial) _status.value = LoadApiStatus.ERROR
+                    null
+                }
+            }
+        }
+    }
+
+    fun refresh() {
+        if (status.value != LoadApiStatus.LOADING) {
+            getAllUsersResult()
+        }
     }
 }
