872750bf309a3770fdaa208e70197bb7b65bedce
aptx113 <aptx113@gmail.com>
2020-04-27 02:01:45 +0800

    Create Adapter for GitHubUser

--- app/src/main/java/com/danteyu/studio/githubusersapp/home/HomeFragment.kt
+++ app/src/main/java/com/danteyu/studio/githubusersapp/home/HomeFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.danteyu.studio.githubusersapp.databinding.FragmentHomeBinding
 import com.danteyu.studio.githubusersapp.ext.getVmFactory
+import com.danteyu.studio.githubusersapp.network.LoadApiStatus
 
 /**
  * Created by George Yu on 2020/4/26.
@@ -25,6 +27,19 @@ class HomeFragment : Fragment() {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this.viewLifecycleOwner
         binding.viewModel = viewModel
+
+        binding.recyclerHome.adapter = HomeAdapter(HomeAdapter.OnClickListener {})
+
+        binding.layoutSwipeRefreshHome.setOnRefreshListener {
+            viewModel.refresh()
+        }
+
+        viewModel.status.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                if (it != LoadApiStatus.LOADING)
+                    binding.layoutSwipeRefreshHome.isRefreshing = false
+            }
+        })
 
         return binding.root
     }
